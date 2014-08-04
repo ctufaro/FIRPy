732f38ff905ac29152c123e1c236d806017188da
unknown <ctufaro@VM-DEV-DEV3.mhtny.com>
2014-08-04 15:27:24 -0400

    Adding charting proejct

---
+++ FIRPy.ReportEngine/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FIRPy.ChartEngine
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Form1 form = new Form1();
+            Application.Run(form);
+        }
+    }
+}
